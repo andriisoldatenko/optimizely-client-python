f632ca5b557780a50813bc6934a01ff4ae1ef2b6
Andrii Soldatenko <andrii.soldatenko@ethoos.com>
2016-11-21 15:40:35 +0200

    Fixed pep8 errors

--- setup.py
+++ setup.py
@@ -8,9 +8,10 @@ setup(name='optimizely',
       author_email='developers@optimizely.com',
       packages=['optimizely'],
       keywords=['python-sdk', 'optimizely'],
-      download_url='https://github.com/optimizely/optimizely-client-python/tarball/0.3',
+      download_url=
+      'https://github.com/optimizely/optimizely-client-python/tarball/0.3',
       install_requires=[
-        'requests',
+          'requests',
       ],
       zip_safe=False,
       test_suite='nose.collector',
